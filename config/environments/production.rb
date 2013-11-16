c7d8c9a9e6ca01cd54009cb5033c6d588c4c2848
Tomas Varaneckas <tomas.varaneckas@gmail.com>
2013-11-16 15:54:26 +0200

    Add host to email params

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,6 +78,8 @@ DatFace::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+  config.action_mailer.default_url_options = { :host => 'dat-face.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
