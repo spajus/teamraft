9e227c854f1c095f0c8afdd776f1ab14252f7999
Tomas Varaneckas <tomas.varaneckas@gmail.com>
2013-11-17 20:35:34 +0200

    Improve design

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,15 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_filter :set_company
+
   def not_found
     raise ActionController::RoutingError.new('Not Found')
+  end
+
+  private
+
+  def set_company
+    @company = current_person.try :company
   end
 end
--- app/controllers/company_controller.rb
+++ app/controllers/company_controller.rb
@@ -3,6 +3,14 @@ class CompanyController < ApplicationController
   before_filter :authenticate_person!
 
   def show
-    @company = current_person.company
+  end
+
+  def edit
+  end
+
+  def update
+    @company.update_attributes(params.require(:company).permit(:name))
+    flash[:notice] = 'Updated'
+    redirect_to action: :edit
   end
 end
