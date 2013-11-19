3105066ef4f8a34f5a02d5f34e9d040206e0d4ac
Tomas Varaneckas <tomas.varaneckas@gmail.com>
2013-11-19 08:51:06 +0200

    Add person card

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,20 @@
+require 'digest/md5'
 module ApplicationHelper
+
+  def icon(name, title)
+    content_tag(:i, '', class: "fa fa-#{name}", title: title)
+  end
+
+  def photo_url(person, options = {})
+    return person.photo if person.photo
+    hash = Digest::MD5.hexdigest(person.email)
+    "http://www.gravatar.com/avatar/#{hash}?s=#{options[:size] || 200}"
+  end
+
+  def admin_tag(person)
+    icon('asterisk', 'Administrator') if person.admin?
+  end
+
   def error_class(entity, field)
     'has-error' if entity.errors.any? && entity.errors[field].any?
   end
