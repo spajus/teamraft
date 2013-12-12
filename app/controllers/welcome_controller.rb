0d799079f5cd2b84f48541f04e201e8f27d1113a
Tomas Varaneckas <tomas.varaneckas@gmail.com>
2013-12-12 07:30:50 +0200

    Add intro block for the internets. Override using ?preview=1

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,6 +3,7 @@ class WelcomeController < ApplicationController
   skip_before_filter :authenticate_person!
 
   def index
+    return render text: 'soon' unless params[:preview]
     return redirect_to company_path if person_signed_in?
     render layout: 'intro'
   end
