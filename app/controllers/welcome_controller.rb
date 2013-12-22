dc155e10911ac6849eb1969c1f9daf245608633f
Tomas Varaneckas <tomas.varaneckas@gmail.com>
2013-12-22 18:58:41 +0200

    Don't render "soon" if person is logged in

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,7 +3,9 @@ class WelcomeController < ApplicationController
   skip_before_filter :authenticate_person!
 
   def index
-    return render text: 'soon' unless params[:preview] || Rails.env.development?
+    unless person_signed_in?
+      return render text: 'soon' unless params[:preview] || Rails.env.development?
+    end
     return redirect_to company_path if person_signed_in?
     render layout: 'intro'
   end
