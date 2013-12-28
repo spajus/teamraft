6efd0416a697aa524b47c0eeebad93cc38218d5e
Tomas Varaneckas <tomas.varaneckas@gmail.com>
2013-12-28 14:51:32 +0200

    Tip for what to do after signup

--- app/controllers/company_controller.rb
+++ app/controllers/company_controller.rb
@@ -3,6 +3,9 @@ class CompanyController < ApplicationController
   before_filter :verify_admin, only: [:edit, :update]
 
   def show
+    if @company.people.count == 1
+      flash.now[:notice] = "Tip: Create some #{view_context.link_to 'Attribute types', attribute_types_path} and then #{view_context.link_to 'Add or Invite your collegues', person_register_path}".html_safe
+    end
   end
 
   def edit
